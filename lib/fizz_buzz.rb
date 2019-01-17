835a892516dec7170c31dbd7e82b7350577c5466
pilar_washburn <pw325555@gmail.com>
2019-01-17 13:54:26 +0100

    first commit

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(number)
+    if number % 3 == 0 and number % 5 == 0
+        'fizzbuzz'
+
+    elsif number % 3 == 0
+        'fizz'
+    
+    elsif number % 5 == 0
+        'buzz'
+
+    else
+        number
+    end
+end
+
